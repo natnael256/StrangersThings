8ea5d12016227b3a23ad9ce255fe99660b41ebe3
Natnael Gebremeden <Natnaelatnafu25@gmail.com>
2023-09-27 15:46:57 -0500

    no update

---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+function UserProfile() {
+    // Add state for user data and a function to fetch user data from the API
+  
+    return (
+      <div>
+        {/* Render user data and messages */}
+      </div>
+    );
+  }
+  
+  export default UserProfile;
