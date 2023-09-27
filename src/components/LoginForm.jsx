f256672189794c75a290f959565add2f0ac85332
Natnael Gebremeden <Natnaelatnafu25@gmail.com>
2023-09-27 15:17:34 -0500

    registration form complete

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,12 +1,15 @@
-const BASE_URL = 'https://strangers-things.herokuapp.com/api/2302-ACC-PT-WEB-PT-B';
-
 import React, { useState } from 'react';
+
+const COHORT_NAME = '2302-acc-pt-web-pt-b'
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
 
 function LoginForm() {
   const [formData, setFormData] = useState({
-    username: '',
-    password: '',
+    username: "",
+    password: "",
   });
+
+  const [loginError, setLoginError] = useState(null);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -19,57 +22,55 @@ function LoginForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(`${BASE_URL}/users/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          user: {
-            username: formData.username,
-            password: formData.password,
+      const response = await fetch(`${BASE_URL}/users/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
           },
-        }),
-      });
+          body: JSON.stringify({
+            user: formData,
+          }),
+        }
+      );
       if (response.ok) {
         const data = await response.json();
         const token = data.data.token;
-        // Handle token (e.g., store it)
+        localStorage.setItem("token", token);
       } else {
-        console.error('Login failed');
+        const errorData = await response.json();
+        setLoginError(errorData.error.message);
       }
     } catch (error) {
-      console.error('Error logging in:', error);
+      console.error("Error logging in user:", error);
     }
   };
 
-  const handleLogout = () => {
-    // Clear the token (e.g., from state and sessionStorage)
-  };
-
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          value={formData.username}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-        <button type="submit">Login</button>
-      </form>
-      <button onClick={handleLogout}>Logout</button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      {loginError && (
+        <p className="text-red-500">{loginError}</p>
+      )}
+      <input
+        type="text"
+        name="username"
+        placeholder="Username"
+        value={formData.username}
+        onChange={handleChange}
+        required
+        minLength="1"
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={formData.password}
+        onChange={handleChange}
+        required
+        minLength="1"
+      />
+      <button type="submit">Login</button>
+    </form>
   );
 }
 
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
+const COHORT_NAME = "2302-acc-pt-web-pt-b";
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
 
 function RegistrationForm() {
   const [formData, setFormData] = useState({
-    username: '',
-    password: '',
-    
+    username: "",
+    password: "",
+    passwordConfirmation: "",
   });
 
-  const [registrationError, setRegistrationError] = useState(null); // Add state for error
+  const [registrationError, setRegistrationError] = useState(null);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -19,30 +22,48 @@ function RegistrationForm() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (formData.password !== formData.passwordConfirmation) {
+      setRegistrationError("Passwords do not match");
+      return;
+    }
     try {
-      const response = await fetch('https://strangers-things.herokuapp.com/api/2302-ACC-PT-WEB-PT-B/users/register', {
-        method: 'POST',
+      const response = await fetch(`${BASE_URL}/users/register`, {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify({
+          user: {
+            username: formData.username,
+            password: formData.password,
+          },
+        }),
       });
       if (response.ok) {
         const data = await response.json();
-        const token = data;
-        // Handle token 
+        const token = data.data.token;
+        localStorage.setItem("token", token);
+
+        // Fetch user data thsi will get the users infor form the API
+        const userDataResponse = await fetch(`${BASE_URL}/users/me`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const userData = await userDataResponse.json();
+        console.log(userData);
       } else {
         const errorData = await response.json();
-        setRegistrationError(errorData.error); // Set the registration error
+        setRegistrationError(errorData.error.message);
       }
     } catch (error) {
-      console.error('Error registering user:', error);
+      console.error("Error registering user:", error);
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      {/* Display registration error */}
       {registrationError && <p className="text-red-500">{registrationError}</p>}
       <input
         type="text"
@@ -51,6 +72,7 @@ function RegistrationForm() {
         value={formData.username}
         onChange={handleChange}
         required
+        minLength="1"
       />
       <input
         type="password"
@@ -59,6 +81,16 @@ function RegistrationForm() {
         value={formData.password}
         onChange={handleChange}
         required
+        minLength="1"
+      />
+      <input
+        type="password"
+        name="passwordConfirmation"
+        placeholder="Confirm Password"
+        value={formData.passwordConfirmation}
+        onChange={handleChange}
+        required
+        minLength="1"
       />
       <button type="submit">Register</button>
     </form>
